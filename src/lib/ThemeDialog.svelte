3146db80bf81927ef8c32f263f3063b2c115bc68
gllms <willemvanderdoe@hotmail.com>
2024-01-03 23:20:55 +0100

    Update ThemeDialog

--- src/lib/ThemeDialog.svelte
+++ src/lib/ThemeDialog.svelte
@@ -4,62 +4,45 @@
   import Select from "./Select.svelte";
 
   export let element: HTMLDialogElement;
+
+  let colors = [
+    "#FF6868",
+    "#FFCB65",
+    "#FFF969",
+    "#86FF72",
+    "#6EFFEE",
+    "#7E9AFF",
+    "#D273FF",
+    "#FC77FF",
+    "#4A3A11",
+    "#FFFFFF",
+    "#313131",
+    "#19197C",
+    "#FA6400",
+  ];
 </script>
 
 <Dialog bind:element drawer>
   <div slot="content" class="flex flex-col gap-4">
-    <div>
-      <p class="text-subtitle-2">Koppen</p>
-      <div class="flex gap-4">
-        <Select value="Graphik" class="flex-1">
-          <option value="Graphik">Graphik</option>
-        </Select>
-        <span class="b-l-(1 solid grey-400)"></span>
-        <Select value="klein">
-          <option value="klein">Klein</option>
-          <option value="middel">Middel</option>
-          <option value="groot">Groot</option>
-        </Select>
+    {#each ["Koppen", "Tekst", "Vragen"] as title}
+      <div>
+        <p class="text-subtitle-2">{title}</p>
+        <div class="flex gap-4">
+          <Select value="Graphik" class="flex-1">
+            <option value="Graphik">Graphik</option>
+          </Select>
+          <span class="b-l-(1 solid grey-400)"></span>
+          <Select value="klein">
+            <option value="klein">Klein</option>
+            <option value="middel">Middel</option>
+            <option value="groot">Groot</option>
+          </Select>
+        </div>
       </div>
-    </div>
-    <hr />
-    <div>
-      <p class="text-subtitle-2">Tekst</p>
-      <div class="flex gap-4">
-        <Select value="Graphik" class="flex-1">
-          <option value="Graphik">Graphik</option>
-        </Select>
-        <span class="b-l-(1 solid grey-400)"></span>
-        <Select value="klein">
-          <option value="klein">Klein</option>
-          <option value="middel">Middel</option>
-          <option value="groot">Groot</option>
-        </Select>
-      </div>
-    </div>
-    <hr />
-    <div>
-      <p class="text-subtitle-2">Vragen</p>
-      <div class="flex gap-4">
-        <Select value="Graphik" class="flex-1">
-          <option value="Graphik">Graphik</option>
-        </Select>
-        <span class="b-l-(1 solid grey-400)"></span>
-        <Select value="klein">
-          <option value="klein">Klein</option>
-          <option value="middel">Middel</option>
-          <option value="groot">Groot</option>
-        </Select>
-      </div>
-    </div>
-    <hr />
+      <hr />
+    {/each}
     <div>
       <p class="text-subtitle-2">Logo</p>
-      <Button secondary icon="add" text="Afbeelding toevoegen" />
-    </div>
-    <hr />
-    <div>
-      <p class="text-subtitle-2">Banner</p>
       <Button secondary icon="add" text="Afbeelding toevoegen" />
     </div>
     <hr />
@@ -67,15 +50,22 @@
       <p class="text-subtitle-2">Achtergrond</p>
       <Button secondary icon="add" text="Afbeelding toevoegen" />
     </div>
-    <hr />
-    <div>
-      <p class="text-subtitle-2">Kleuren</p>
-      <div class="flex gap-2">
-        {#each ["#FF6868", "#FFCB65", "#FFF969", "#86FF72", "#6EFFEE", "#7E9AFF"] as color}
-          <button style:background-color={color} class="w-16 h-16 p-0 b-none rd-2 cursor-pointer"></button>
-        {/each}
+    {#each ["Primaire kleur", "Secundaire kleur"] as titel}
+      <hr />
+      <div>
+        <p class="text-subtitle-2">{titel}</p>
+        <div class="flex flex-wrap gap-2">
+          {#each colors as color}
+            <button
+              style:background-color={color}
+              class="w-16 h-16 p-0 b-none rd-2 cursor-pointer b-(1 solid transparent)"
+              class:!b-black={color === "#FFFFFF"}
+            ></button>
+          {/each}
+          <button class="icon-button w-16 h-16 b-none rd-2">add</button>
+        </div>
       </div>
-    </div>
+    {/each}
   </div>
   <svelte:fragment slot="footer">
     <Button class="ml-auto" icon="check" text="Thema opslaan" />
