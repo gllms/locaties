66d0bb79bb365f0e6b8a6785bf8493449c117635
gllms <willemvanderdoe@hotmail.com>
2024-01-10 09:29:04 +0100

    Remove inconsistent cursor

--- src/lib/PhoneFillInPage.svelte
+++ src/lib/PhoneFillInPage.svelte
@@ -18,14 +18,3 @@
     <ViewMode {title} {description} {canvasItems} />
   </div>
 </div>
-
-<style>
-  ._backdrop {
-    /* cursor is a svg data url of two concentric semi-transparent black circles */
-    cursor:
-      url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Ccircle cx='8' cy='8' r='8' fill='rgba(0,0,0,.2)'/%3E%3Ccircle cx='8' cy='8' r='6.5' fill='rgba(0,0,0,.2)'/%3E%3C/svg%3E")
-        16 16,
-      auto !important;
-  }
-
-</style>
